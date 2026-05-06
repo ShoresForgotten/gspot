5e3df8d2742ff237f5322ecb0485f0799e6e4df3
ShoresForgotten <spiral@shoresforgotten.com>
2026-05-06 16:15:12 +0200

    Remove final raw command project setting
    Also reset changed debug settings, since I'm not the maintainer on this
    project

--- addons/gspot/gsconstants.gd
+++ addons/gspot/gsconstants.gd
@@ -8,5 +8,4 @@ const CLIENT_VERSION: String = "3.0.0"
 const MESSAGE_RATE: float = 0.2
 const PROJECT_SETTING_CLIENT_NAME: String = "gspot/client/client_name"
 const PROJECT_SETTING_CLIENT_VERSION: String = "gspot/client/client_version"
-const PROJECT_SETTING_ENABLE_RAW_COMMANDS: String = "gspot/client/enable_raw_commands"
 const PROJECT_SETTINGS_MESSAGE_RATE: String = "gspot/client/message_rate"
